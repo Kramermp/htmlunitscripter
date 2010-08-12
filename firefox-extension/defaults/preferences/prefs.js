f62702c0f1fc73a5ca8b39916c11c10144f2a89b
grossm04 <grossm04@aec46e49-5bd6-2649-22a7-033e6d80cc03>
2010-08-12 05:01:46 +0000

    Initial commit for version 1.0.0

---
+++ firefox-extension/defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.htmlunitscripter.logging", 1);
+pref("extensions.htmlunitscripter.printProgress", true);
+pref("extensions.htmlunitscripter.useMainFrameOnly", true);
+pref("extensions.htmlunitscripter.linkPreference", "link-useIdOrName");
+pref("extensions.htmlunitscripter.elementPreference", "element-useIdOrName");
+pref("extensions.htmlunitscripter.elementWithoutValuePreference", "element-useIdOrName");
