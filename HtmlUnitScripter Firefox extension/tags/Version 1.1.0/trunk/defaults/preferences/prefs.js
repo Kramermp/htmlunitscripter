8cf8d7dc1a5ab4fa835fe89c3dce3c2d0b21baf6
grossm04 <grossm04@aec46e49-5bd6-2649-22a7-033e6d80cc03>
2010-12-12 23:34:56 +0000

    Tag for version 1.1.0

---
+++ HtmlUnitScripter Firefox extension/tags/Version 1.1.0/trunk/defaults/preferences/prefs.js
@@ -0,0 +1,5 @@
+pref("extensions.htmlunitscripter.logging", 1);
+pref("extensions.htmlunitscripter.printProgress", true);
+pref("extensions.htmlunitscripter.useMainFrameOnly", true);
+pref("extensions.htmlunitscripter.linkPreference", "link-useId");
+pref("extensions.htmlunitscripter.elementPreference", "element-useId");
