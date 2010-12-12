acf09765cc3d597157cb1134a0ec7af71547090a
grossm04 <grossm04@aec46e49-5bd6-2649-22a7-033e6d80cc03>
2010-12-12 23:42:32 +0000

    Deleting 1.1.0 tag again

--- HtmlUnitScripter Firefox extension/tags/Version 1.1.0/trunk/trunk/defaults/preferences/prefs.js
+++
@@ -1,5 +0,0 @@
-pref("extensions.htmlunitscripter.logging", 1);
-pref("extensions.htmlunitscripter.printProgress", true);
-pref("extensions.htmlunitscripter.useMainFrameOnly", true);
-pref("extensions.htmlunitscripter.linkPreference", "link-useId");
-pref("extensions.htmlunitscripter.elementPreference", "element-useId");
